2c369f862e2c670c15709d4348960998e8fe317c
TheDailyM <tomekkaniuk99@gmail.com>
2025-04-23 14:54:21 +0200

    zrobić slider

--- app/src/main/java/com/example/wlasciwosci_czcionki/MainActivity.kt
+++ app/src/main/java/com/example/wlasciwosci_czcionki/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.wlasciwosci_czcionki
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+lateinit var spinner: SeekBar;
+lateinit var button: Button
+var wielkosc_czcionki: Int = 14
+lateinit var pole: TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +25,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        spinner = findViewById(R.id.Spinner)
+        button = findViewById(R.id.Button_next)
+        pole = findViewById(R.id.Text_pole)
+
+        val powitanie = arrayOf("Dzień dobry", "Good Morning", "Buenos dias");
+
+        spinner.setOnSeekBarChangeListener( object : SeekBar.OnSeekBarChangeListener {
+            // Handle when the progress changes
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {}
+
+            // Handle when the user starts tracking touch
+            override fun onStartTrackingTouch(seek: SeekBar) {}
+
+            // Handle when the user stops tracking touch
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+                // miejsce na mój kod!!
+
+                val output = "Progress is: " + seek.progress + "%"
+                Toast.makeText(this@MainActivity, output , Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 }
